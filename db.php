01a465eb815f92277679e92e48a71b9c554ae7e6
Jean Eba <jeba760@gmail.com>
2023-03-28 01:41:53 +0000

    update password

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 try {
-	$db = new PDO("mysql:host=localhost;dbname=isga;charset=utf8",'jahno','jahno123');
+	$db = new PDO("mysql:host=localhost;dbname=isga;charset=utf8",'jahno','jahno1');
 } catch (Exception $e) {
 	die($e->getMessage());
 }
